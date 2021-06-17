10294addd2096b32422c82f8b926ec6ba93949e0
msrocka <michael.srocka@gmail.com>
2021-06-17 13:01:19 +0200

    compile as UMD

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,29 @@
 const path = require('path');
-const buildDir = path.resolve(__dirname, 'dist');
 
-const config = {
-    entry: {
-        'useeio': './src/useeio.ts',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: '[name].js',
-        path: buildDir,
-        libraryTarget: 'var',
-        library: '[name]',
-    },
-};
-
-module.exports = (_env, argv) => {
-    if (argv.mode === 'development') {
-        config.devtool = 'source-map';
-    }
-    return config;
+module.exports = {
+  entry: {
+    'useeio-api': './src/webapi.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'useeio',
+    umdNamedDefine: true,
+    //globalObject: 'this',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  
 };
