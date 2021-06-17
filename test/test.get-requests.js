7b2f4b416d5e8327e6c24b9bdf9ff0088c833ca0
msrocka <michael.srocka@gmail.com>
2021-06-17 20:47:28 +0200

    more checks, less boilerplate

--- test/test.get-requests.js
+++ test/test.get-requests.js
@@ -4,16 +4,6 @@
 /** @type WebApi */
 var webApi = webApi;
 
-/**
- * @param {(model: WebModel) => void} fn the model callback
- */
-async function eachModel(fn) {
-  for (const info of await webApi.getModelInfos()) {
-    /** @type WebModel */
-    const model = new useeio.WebModel(webApi, info.id);
-    fn(model);
-  }
-}
 
 describe("model requests", () => {
 
@@ -28,41 +18,73 @@ describe("model requests", () => {
     }
   });
 
-  it("WebModel.getSectors", async () => {
-    for (const info of await webApi.getModelInfos()) {
-      /** @type WebModel */
-      const model = new useeio.WebModel(webApi, info.id);
+  it("WebModel.sectors", async () => {
+    await eachModel(async model => {
       const sectors = await model.sectors();
       assert.isTrue(sectors.length > 0);
-      console.log(`fetched ${sectors.length} sectors from model ${info.id}`)
-      let i = 0;
-      for (const sector of sectors) {
-        console.log(`  - ${sector.code} - ${sector.name}`)
-        i++;
-        if (i > 4) {
-          console.log(`  - ...`);
-          break;
-        }
-      }
-    }
+      console.log(`fetched ${sectors.length} sectors from model ${model.id()}`)
+      printFirstOf(sectors, sector => {
+        chai.assert.isOk(sector.id);
+        chai.assert.isOk(sector.code);
+        chai.assert.isOk(sector.name);
+        return `${sector.code} - ${sector.name}`;
+      });
+    });
   });
 
-  it("WebModel.getIndicators", async () => {
+  it("WebModel.indicators", async () => {
     await eachModel(async model => {
       const indicators = await model.indicators();
       const n = indicators.length;
       assert.isTrue(n > 0);
       console.log(`fetched ${n} indicators from model ${model.id()}`);
-      let i = 0;
-      for (const indicator of indicators) {
-        console.log(`  - ${indicator.code} - ${indicator.name}`);
-        i++;
-        if (i > 4) {
-          console.log(`  - ...`);
-          break;
-        }
-      }
-    })
+      printFirstOf(indicators, indicator => {
+        chai.assert.isOk(indicator.id);
+        chai.assert.isOk(indicator.code);
+        chai.assert.isOk(indicator.name);
+        return `${indicator.code} - ${indicator.name}`;
+      });
+    });
+  });
+
+  it("WebModel.demands", async () => {
+    await eachModel(async model => {
+      const demandInfos = await model.demands();
+      const n = demandInfos.length;
+      assert.isTrue(n > 0);
+      console.log(`fetched ${n} demand infos from model ${model.id()}`)
+      printFirstOf(demandInfos, info => {
+        chai.assert.isOk(info.id);
+        return info.id;
+      });
+    });
   });
 });
 
+/**
+ * @param {(model: WebModel) => void} fn the model callback
+ */
+async function eachModel(fn) {
+  for (const info of await webApi.getModelInfos()) {
+    /** @type WebModel */
+    const model = new useeio.WebModel(webApi, info.id);
+    fn(model);
+  }
+}
+
+/**
+ * @param {T[]} xs the array
+ * @param {(x: T) => string} fn the function that maps an element to a string
+ * @template T
+ */
+function printFirstOf(xs, fn) {
+  let i = 0;
+  for (const x of xs) {
+    console.log(`  - ${fn(x)}`);
+    i++;
+    if (i > 4 && i < xs.length) {
+      console.log(`  - ..`);
+      break;
+    }
+  }
+}
